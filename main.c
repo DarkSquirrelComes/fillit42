3d96c98af52602fe53795e387bf15f1cc61cc296
Alfyn Kittie <akittie@su-j6.21-school.ru>
2019-11-25 18:59:25 +0300

    Managed error with more than 26 tetrs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akittie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 13:48:11 by akittie           #+#    #+#             */
-/*   Updated: 2019/11/25 18:53:19 by akittie          ###   ########.fr       */
+/*   Updated: 2019/11/25 18:58:57 by akittie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int		main(int ac, char **av)
 	int			n_tetr;
 	t_tetr		*my_tetr;
 
-	if ((n_tetr = num_valid(ac, av)) <= 0)
+	if ((n_tetr = num_valid(ac, av)) <= 0 || n_tetr > 26)
 		return (0);
 	if (!(my_tetr = get_tetr(av[1], n_tetr)))
 		return (ft_error());
